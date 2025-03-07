92ab9c00960aa096b62a64f0c466cfada3c1de4b
Artem Dogadailo <artem.dogadailo@student.griffith.ie>
2025-03-07 22:21:44 +0000

    creating unit test method called testStringToInteger, adding required values for the test

--- src/griffith/ConversionTest.java
+++ src/griffith/ConversionTest.java
@@ -21,11 +21,21 @@ public class ConversionTest {
 	@Test
 	public void testDollarToEuro() {
 		Conversions conv = new Conversions();
-		
+
 		// examples for conversion rates
-		assertEquals(0.91, conv.dollarToEuro(1.0), 0.01); 
+		assertEquals(0.91, conv.dollarToEuro(1.0), 0.01);
 		assertEquals(9.1, conv.dollarToEuro(10.0), 0.01);
 		assertEquals(0.0, conv.dollarToEuro(0.0), 0.01);
 	}
-
+	
+	
+	// testing String to integer method
+	@Test
+	public void testStringToInteger() {
+		Conversions conv = new Conversions();
+		//adding required examples to test different values
+		assertEquals(123, conv.stringToInteger("123"));
+		assertEquals(-45, conv.stringToInteger("-45"));
+		assertThrows(NumberFormatException.class, () -> conv.stringToInteger("abc"));
+	}
 }
