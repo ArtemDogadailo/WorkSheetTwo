02203cca38eebda40a8af4d54202db315283ffea
Artem Dogadailo <artem.dogadailo@student.griffith.ie>
2025-03-07 22:19:40 +0000

    creating method that tests method called testDollarToEuro method, testing method with required examples

--- src/griffith/ConversionTest.java
+++ src/griffith/ConversionTest.java
@@ -5,16 +5,27 @@ import org.junit.jupiter.api.Test;
 
 public class ConversionTest {
 
-    @Test
-    
-    //testing euro to dollar conversion
-    public void testEuroToDollar() {
-        Conversions conv = new Conversions();
-        
-        //examples for conversion rates
-        assertEquals(1.1, conv.euroToDollar(1.0), 0.01);  
-        assertEquals(11.0, conv.euroToDollar(10.0), 0.01);
-        assertEquals(0.0, conv.euroToDollar(0.0), 0.01);
-    }
+	@Test
+
+	// testing euro to dollar conversion
+	public void testEuroToDollar() {
+		Conversions conv = new Conversions();
+
+		// examples for conversion rates
+		assertEquals(1.1, conv.euroToDollar(1.0), 0.01);
+		assertEquals(11.0, conv.euroToDollar(10.0), 0.01);
+		assertEquals(0.0, conv.euroToDollar(0.0), 0.01);
+	}
+
+	// testing dollar to euro conversion
+	@Test
+	public void testDollarToEuro() {
+		Conversions conv = new Conversions();
+		
+		// examples for conversion rates
+		assertEquals(0.91, conv.dollarToEuro(1.0), 0.01); 
+		assertEquals(9.1, conv.dollarToEuro(10.0), 0.01);
+		assertEquals(0.0, conv.dollarToEuro(0.0), 0.01);
+	}
 
 }
