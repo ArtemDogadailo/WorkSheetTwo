315573ac9e0032ca05dd33dae8a732d4cf86c8a2
Artem Dogadailo <artem.dogadailo@student.griffith.ie>
2025-03-07 22:07:49 +0000

    creating stub methods for the methods in conversions java class

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -2,4 +2,31 @@ package griffith;
 
 public class Conversions {
 
+	// creating stub methods for the task one
+
+	// creating method for euro to dollar convertion
+	public double euroToDollar(double euro) {
+		return 0.0;
+
+	}
+
+	// creating method for dollar to euro convertion
+	public double dollarToEuro(double dollar) {
+		return 0.0;
+	}
+
+	// creating method that converts String to Integer
+	public int stringToInteger(String val) {
+		return 0;
+	}
+
+	// creating method that converts Integer to String
+	public String integerToString(int val) {
+		return "";
+	}
+
+	// creating method that switches uppercase letters to lowercase latters and vice versa
+	public String switchCase() {
+		return "";
+	}
 }
