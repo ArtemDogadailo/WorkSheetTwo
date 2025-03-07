8dfa1724dba2f025b9a3733090c91fe88771b97c
Artem Dogadailo <artem.dogadailo@student.griffith.ie>
2025-03-07 22:38:52 +0000

    implementing all the methods in convertion class

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -6,27 +6,50 @@ public class Conversions {
 
 	// creating method for euro to dollar convertion
 	public double euroToDollar(double euro) {
-		return 0.0;
+		//adding example rate 1 to 1.1
+		return euro * 1.1;
 
 	}
 
 	// creating method for dollar to euro convertion
 	public double dollarToEuro(double dollar) {
-		return 0.0;
+		//adding example rate 1 to 0.91
+		return dollar * 0.91;
 	}
 
 	// creating method that converts String to Integer
 	public int stringToInteger(String val) {
-		return 0;
+		//implementing convertion with parse method
+		return Integer.parseInt(val);
 	}
 
 	// creating method that converts Integer to String
 	public String integerToString(int val) {
-		return "";
+		//implementing convertion to string using valueOf method
+		return String.valueOf(val);
 	}
 
 	// creating method that switches uppercase letters to lowercase latters and vice versa
-	public String switchCase() {
-		return "";
+	public String switchCase(String input) {
+		//implementing switch case method using String Buildre
+		StringBuilder result = new StringBuilder();
+		
+		//break down the string into the array of characters 
+		for (char c : input.toCharArray()) {
+			//if the character is uppercase, it will be converted to lowercase
+            if (Character.isUpperCase(c)) {
+                result.append(Character.toLowerCase(c));
+            }
+            //in other case the character is lowercase, consequently it will be converted to UpperCase
+            else if (Character.isLowerCase(c)) {
+                result.append(Character.toUpperCase(c));
+            }
+            //adding character to the future string
+            else {
+                result.append(c);
+            }
+        }
+		
+		return result.toString();
 	}
 }
